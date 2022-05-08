045a8bfe87c198ae68324284010ded1df469ffa7
Abobakr Mohammed <abobakrmohammed4@gmail.com>
2022-05-08 10:04:01 +0200

    update

--- BookingHotel.Interface/Configurations/CreateClient.cs
+++ BookingHotel.Interface/Configurations/CreateClient.cs
@@ -3,45 +3,28 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace BookingHotel.Interface.Configurations
 {
     public class CreateClient
     {
-        public static IRestClient _restClient;
+        public static RestClient _restClient;
         public static void IntializeClientRequest()
         {
-            _restClient = new RestClient();
-            _restClient.BaseUrl = new Uri(@"https://localhost:44385/");
-            //var certStore = new X509Store(StoreName.Root, StoreLocation.LocalMachine); //replace with appropriate values based on your cert configuration
-            //certStore.Open(OpenFlags.ReadOnly);
-            ////var cert = certStore.Certificates.Find(X509FindType.FindByThumbprint, "<cert Thumbprint>", false);
-            //var ecdsa = ECDsa.Create(); // generate asymmetric key pair
-
-            //var req = new CertificateRequest("cn=foobar", ecdsa, HashAlgorithmName.SHA256);
-            //var cert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(10));
-            //// Create PFX (PKCS #12) with private key
-            //File.WriteAllBytes("c:\\temp\\mycert.pfx", cert.Export(X509ContentType.Pfx, "P@55w0rd"));
-
-            //// Create Base 64 encoded CER (public key only)
-            //File.WriteAllText("c:\\temp\\mycert.cer",
-            //    "-----BEGIN CERTIFICATE-----\r\n"
-            //    + Convert.ToBase64String(cert.Export(X509ContentType.Cert), Base64FormattingOptions.InsertLineBreaks)
-            //    + "\r\n-----END CERTIFICATE-----");
-            //_restClient.ClientCertificates = new X509CertificateCollection();
-            //_restClient.ClientCertificates.Add(new X509Certificate2("c:\\temp\\mycert.cer"));
+            _restClient = new RestClient(@"http://localhost:44385/");
         }
-        public static IRestResponse SendRestPostRequest(string resource, Method method ,string token= "", string body = "")
+        public static async Task<RestResponse> SendRestPostRequest(string resource, Method method ,string token= "", string body = "")
         {
             IntializeClientRequest();
-            IRestRequest request = new RestRequest();
-            request.Resource = new Uri(_restClient.BaseUrl + resource).ToString();
+            RestRequest request = new RestRequest();
+            request.Resource = new Uri(@"https://localhost:44385/" + resource).ToString();
             request.Method = method;
             request.RequestFormat = DataFormat.Json;
-            request.AddHeader("content-type", "application/json");
+            request.AddHeader("content-type", "text/plain");
 //            request.AddHeader("Authorization", "Bearer " + token);
 
-            var response = _restClient.Execute(method == Method.GET ? request : request.AddJsonBody(body));
+            var response = await _restClient.ExecuteAsync(method == Method.Get ? request : request.AddJsonBody(body));
 
             return response;
         }
--- BookingHotel.Interface/Controllers/BookingController.cs
+++ BookingHotel.Interface/Controllers/BookingController.cs
@@ -3,14 +3,15 @@ using BookingHotel.Interface.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BookingHotel.Interface.Controllers
 {
     public class BookingController : Controller
     {
-        public IActionResult Book()
+        public async Task<IActionResult> Book()
         {
-            var hotel = CreateClient.SendRestPostRequest("api/hotel", RestSharp.Method.GET ,"", null);
+            var hotel = await CreateClient.SendRestPostRequest("api/hotel", RestSharp.Method.Get ,"", null);
             var content = hotel.Content;
             var hotels = JsonConvert.DeserializeObject<List<HotelModel>>(content);
             BookingModel model = new BookingModel() { HotelModel = hotels };
